537d02ccb1a60234ea2c0f9f6fa565a9ae7f71f1
minfrin <minfrin@users.noreply.github.com>
2020-06-16 22:43:19 +0200

    Add initial skeleton.

--- mod_dav_calendar.c
+++ mod_dav_calendar.c
@@ -15,3 +15,90 @@
  */
 
 
+/*
+ * The Apache mod_dav_calendar module adds CalDav support.
+ *
+ *  Author: Graham Leggett
+ *
+ */
+#include <apr_lib.h>
+
+#include "httpd.h"
+#include "http_config.h"
+#include "http_core.h"
+#include "http_log.h"
+#include "http_protocol.h"
+#include "http_request.h"
+#include "util_script.h"
+
+module AP_MODULE_DECLARE_DATA dav_calendar_module;
+
+typedef struct
+{
+    int dav_calendar_set :1;
+    int dav_calendar;
+} dav_calendar_config_rec;
+
+static void *create_dav_calendar_dir_config(apr_pool_t *p, char *d)
+{
+	dav_calendar_config_rec *conf = apr_pcalloc(p, sizeof(dav_calendar_config_rec));
+
+    return conf;
+}
+
+static void *merge_dav_calendar_dir_config(apr_pool_t *p, void *basev, void *addv)
+{
+	dav_calendar_config_rec *new = (dav_calendar_config_rec *) apr_pcalloc(p,
+            sizeof(dav_calendar_config_rec));
+	dav_calendar_config_rec *add = (dav_calendar_config_rec *) addv;
+	dav_calendar_config_rec *base = (dav_calendar_config_rec *) basev;
+
+    new->dav_calendar = (add->dav_calendar_set == 0) ? base->dav_calendar : add->dav_calendar;
+    new->dav_calendar_set = add->dav_calendar_set || base->dav_calendar_set;
+
+    return new;
+}
+
+static const char *set_dav_calendar(cmd_parms *cmd, void *dconf, int flag)
+{
+    dav_calendar_config_rec *conf = dconf;
+
+    conf->dav_calendar = flag;
+    conf->dav_calendar_set = 1;
+
+    return NULL;
+}
+
+static const command_rec dav_calendar_cmds[] =
+{
+    AP_INIT_FLAG("DavCalendar",
+        set_dav_calendar, NULL, RSRC_CONF | ACCESS_CONF,
+        "When enabled, the URL space will support calendars."),
+    { NULL }
+};
+
+static int dav_calendar_handler(request_rec *r)
+{
+
+	dav_calendar_config_rec *conf = ap_get_module_config(r->per_dir_config,
+            &dav_calendar_module);
+
+    return DECLINED;
+
+}
+
+static void register_hooks(apr_pool_t *p)
+{
+    ap_hook_handler(dav_calendar_handler, NULL, NULL, APR_HOOK_MIDDLE);
+}
+
+AP_DECLARE_MODULE(dav_calendar) =
+{
+    STANDARD20_MODULE_STUFF,
+    create_dav_calendar_dir_config, /* dir config creater */
+    merge_dav_calendar_dir_config,  /* dir merger --- default is to override */
+    NULL,                           /* server config */
+    NULL,                           /* merge server config */
+    dav_calendar_cmds,              /* command apr_table_t */
+    register_hooks                  /* register hooks */
+};
